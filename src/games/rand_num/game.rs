7fc2b0c983edc0ab6711b4b1a7365df2a047fcde
Guy Cohen <110805121+guyco3@users.noreply.github.com>
2026-01-06 03:13:30 -0800

    docs

--- src/games/rand_num/game.rs
+++ src/games/rand_num/game.rs
@@ -42,35 +42,22 @@ impl Game for NumberGame {
     fn handle_input(&mut self, event: crossterm::event::KeyEvent, ctx: &Context<Self::Message>) {
         if self.game_over { return; }
 
-        // Log keys to stderr for debugging input capture.
-        eprintln!("Key event: {:?}", event.code);
-
         match event.code {
             crossterm::event::KeyCode::Char(c) => {
                 if c.is_ascii_digit() {
                     self.current_input.push(c);
-                } else {
-                    eprintln!("[INPUT] Ignored non-digit: {}", c);
                 }
             }
             crossterm::event::KeyCode::Backspace => {
                 self.current_input.pop();
             }
             crossterm::event::KeyCode::Enter => {
-                if self.current_input.is_empty() {
-                    eprintln!("[INPUT] No input to send!");
-                    return;
-                }
                 if let Ok(guess) = self.current_input.parse::<u32>() {
                     if !self.is_host {
                         // CLIENT: Send the guess to the host
-                        eprintln!("[CLIENT] Sending guess: {}", guess);
                         ctx.send_network_event(GuessMsg::PlayerGuessed(guess));
-                        self.last_feedback = format!("[SENT] You guessed {}. Waiting for feedback...", guess);
+                        self.last_feedback = format!("You guessed {}. Waiting for feedback...", guess);
                     }
-                } else {
-                    eprintln!("[INPUT] Failed to parse: {}", self.current_input);
-                    self.last_feedback = "Invalid number!".into();
                 }
                 self.current_input.clear();
             }
@@ -79,20 +66,16 @@ impl Game for NumberGame {
     }
 
     fn handle_network(&mut self, msg: Self::Message, ctx: &Context<Self::Message>) {
-        eprintln!("[NETWORK] Received message: {:?}", msg);
         match msg {
             GuessMsg::PlayerGuessed(guess) => {
                 if self.is_host {
                     // HOST: Check the guess and send feedback
-                    eprintln!("[HOST] Player guessed: {}, secret: {}", guess, self.secret_number);
+                    println!("Client guessed: {}", guess);
                     if guess < self.secret_number {
-                        eprintln!("[HOST] Too low. Sending feedback.");
                         ctx.send_network_event(GuessMsg::Feedback("Too low!".into()));
                     } else if guess > self.secret_number {
-                        eprintln!("[HOST] Too high. Sending feedback.");
                         ctx.send_network_event(GuessMsg::Feedback("Too high!".into()));
                     } else {
-                        eprintln!("[HOST] Correct! Sending game over.");
                         ctx.send_network_event(GuessMsg::GameOver(guess));
                         self.game_over = true;
                         self.last_feedback = format!("Player won! Number was {}", guess);
@@ -100,11 +83,9 @@ impl Game for NumberGame {
                 }
             }
             GuessMsg::Feedback(text) => {
-                eprintln!("[CLIENT] Received feedback: {}", text);
                 self.last_feedback = text;
             }
             GuessMsg::GameOver(num) => {
-                eprintln!("[CLIENT] Game over: {}", num);
                 self.game_over = true;
                 self.last_feedback = format!("Correct! The number was {}", num);
             }
